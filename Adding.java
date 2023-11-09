e44cb4ee070fcaf57beb7003413ff4d47c2001c5
Asuran7596 <111649751+Asuran7596@users.noreply.github.com>
2023-11-09 15:16:00 +0530

    Add files via upload

---
+++ Adding.java
@@ -0,0 +1,13 @@
+package com.venky.adding;
+
+public class Adding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5, b = 5;
+		int sum;
+		sum = a + b;
+		System.out.println("adding numbers " + sum);
+	}
+
+}
